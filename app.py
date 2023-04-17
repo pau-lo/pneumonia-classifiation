6ca3457b8507c533547bcdfabeb6661b30a610cf
Peelo R L <pau_lo_lop_ez@protonmail.com>
2023-04-17 12:03:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ temp_file = NamedTemporaryFile(delete=False)
 # after loading data
 
 st.write(
-    "The photo is done uploading and reshaping.  Now, please hit the Predict" +
-    "button in order to see if whether the images belongs to a pneumonia person"  + 
+    "After the photo is done uploading and reshaping.  Please hit the Predict " +
+    "button in order to see if whether the images belongs to a pneumonia person "  + 
     "or not.")
 
 
